6f1e7f79d2c7a78ecf891a699c84fc57dde814b5
XandersonSilva <isacsilvasouza2@gmail.com>
2025-12-02 20:56:21 -0300

    feat: Adicionando verificação do nome da empresa

--- back/src/main/java/com/include/inovale/landingpage/models/dtos/ReqParceriaDTO.java
+++ back/src/main/java/com/include/inovale/landingpage/models/dtos/ReqParceriaDTO.java
@@ -1,9 +1,11 @@
 package com.include.inovale.landingpage.models.dtos;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class ReqParceriaDTO {
+    @NotBlank
     private String nomeEmpresa;
     private String logoUrl;
     private String siteUrl;

60abf06dc374de6229a94a495d601499469dc9ed
XandersonSilva <isacsilvasouza2@gmail.com>
2025-12-03 18:27:19 -0300

    feat: Validação ReqCasosSucessoDTO

--- back/src/main/java/com/include/inovale/landingpage/models/dtos/ReqCasosSucessoDTO.java
+++ back/src/main/java/com/include/inovale/landingpage/models/dtos/ReqCasosSucessoDTO.java
@@ -1,14 +1,26 @@
 package com.include.inovale.landingpage.models.dtos;
 
 import java.util.UUID;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class ReqCasosSucessoDTO {
+    @NotBlank
     private String nomeProjeto; 
+
+    @NotBlank
     private String cliente;
+
+    @NotBlank
     private String descricaoSolucao;
+
+    @NotBlank
     private String depoimentoCliente;
     private String imagemUrl;
+
+    @NotNull
     private UUID idServico;
 }
